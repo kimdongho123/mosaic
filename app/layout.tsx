a160d2a7eddfb6d698d04d3bd093907ccb35c367
김동호 <tmzkdlxj480@gmail.com>
2026-06-08 12:40:05 +0900

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Mosiac",
+  description: "Mosiac",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
